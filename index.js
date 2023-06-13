909db366943d80ccef32d7e02b23d689247d0cbb
fengmk2 <fengmk2@gmail.com>
2023-06-13 18:33:16 +0800

    feat: use github action and drop non-lts Node.js support
    BREAKING CHANGE: Drop Node.js < 14 support

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
-/**!
- * sendmessage - index.js
- *
- * Copyright(c) fengmk2 and other contributors.
- * MIT Licensed
- *
- * Authors:
- *   fengmk2 <fengmk2@gmail.com> (http://fengmk2.github.com)
- */
+const { isMainThread, parentPort } = require('worker_threads');
 
-'use strict';
-
-/**
- * Module dependencies.
- */
-var { isMainThread, parentPort } = require('worker_threads');
-
-var IS_NODE_DEV_RUNNER = /node\-dev$/.test(process.env._ || '');
+let IS_NODE_DEV_RUNNER = /node\-dev$/.test(process.env._ || '');
 if (!IS_NODE_DEV_RUNNER && process.env.IS_NODE_DEV_RUNNER) {
   IS_NODE_DEV_RUNNER = true;
 }
@@ -38,24 +23,24 @@ module.exports = function send(child, message) {
 
   // child is worker
   if (typeof child.postMessage === 'function') {
-    return child.postMessage(message)
+    return child.postMessage(message);
   }
   // in worker thread
   if (!isMainThread) {
-    return parentPort.postMessage(message)
+    return parentPort.postMessage(message);
   }
 
   // cluster.fork(): child.process is process
   // childprocess.fork(): child is process
-  var connected = child.process ? child.process.connected : child.connected;
+  const connected = child.process ? child.process.connected : child.connected;
 
   if (connected) {
     return child.send(message);
   }
 
   // just log warnning message
-  var pid = child.process ? child.process.pid : child.pid;
-  var err = new Error('channel closed');
+  const pid = child.process ? child.process.pid : child.pid;
+  const err = new Error('channel closed');
   console.warn('[%s][sendmessage] WARN pid#%s channel closed, nothing send\nstack: %s',
     Date(), pid, err.stack);
 };
--- test/child.js
+++ test/child.js
@@ -1,40 +1,23 @@
-/**!
- * sendmessage - test/child.js
- *
- * Copyright(c) fengmk2 and other contributors.
- * MIT Licensed
- *
- * Authors:
- *   fengmk2 <fengmk2@gmail.com> (http://fengmk2.github.com)
- */
+const { isMainThread, parentPort } = require('worker_threads');
+const sendmessage = require('..');
 
-'use strict';
-
-/**
- * Module dependencies.
- */
-
-var { isMainThread, parentPort } = require('worker_threads');
-var sendmessage = require('../');
-
-
-var listener = function (message) {
+const listener = function(message) {
   if (message.disconnect) {
     process.disconnect();
   }
 
   sendmessage(process, {
     from: 'child',
-    got: message
+    got: message,
   });
-}
+};
 process.on('message', listener);
 if (!isMainThread) {
   // worker thread
-  parentPort.on('message', listener)
+  parentPort.on('message', listener);
 }
 
 sendmessage(process, {
   from: 'child',
-  hi: 'this is a message send to master'
+  hi: 'this is a message send to master',
 });
