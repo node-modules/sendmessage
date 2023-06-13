9592a9bd9fe880b565475d7583448af46da077f4
fengmk2 <fengmk2@gmail.com>
2023-06-13 20:35:17 +0800

    feat: use github action and drop non-lts Node.js support (#4)
    BREAKING CHANGE: Drop Node.js < 14 support

--- test/sendmessage.test.js
+++ test/sendmessage.test.js
@@ -1,89 +1,73 @@
-/**!
- * sendmessage - test/sendmessage.test.js
- *
- * Copyright(c) fengmk2 and other contributors.
- * MIT Licensed
- *
- * Authors:
- *   fengmk2 <fengmk2@gmail.com> (http://fengmk2.github.com)
- */
+const path = require('path');
+require('should');
+const childprocess = require('child_process');
+const cluster = require('cluster');
+const workerThreads = require('worker_threads');
+const mm = require('mm');
+const sendmessage = require('../');
 
-"use strict";
-
-/**
- * Module dependencies.
- */
-
-var path = require('path');
-var should = require('should');
-var childprocess = require('child_process');
-var cluster = require('cluster');
-var workerThreads = require('worker_threads');
-var mm = require('mm');
-var sendmessage = require('../');
-
-describe('sendmessage.test.js', function () {
+describe('sendmessage.test.js', function() {
   afterEach(mm.restore);
 
-  describe('single process', function () {
-    it('should emit message when process is not child process', function (done) {
-      process.once('message', function (message) {
+  describe('single process', function() {
+    it('should emit message when process is not child process', function(done) {
+      process.once('message', function(message) {
         message.should.eql({
-          foo: 'bar'
+          foo: 'bar',
         });
         done();
       });
-      sendmessage(process, {foo: 'bar'});
+      sendmessage(process, { foo: 'bar' });
     });
   });
 
-  describe('child_process.fork()', function () {
-    it('should send cross process message', function (done) {
-      var childfile = path.join(__dirname, 'child.js');
-      var child = childprocess.fork(childfile);
-      child.once('message', function (message) {
+  describe('child_process.fork()', function() {
+    it('should send cross process message', function(done) {
+      const childfile = path.join(__dirname, 'child.js');
+      const child = childprocess.fork(childfile);
+      child.once('message', function(message) {
         message.should.eql({
           from: 'child',
-          hi: 'this is a message send to master'
+          hi: 'this is a message send to master',
         });
 
         sendmessage(child, {
           from: 'master',
-          reply: 'this is a reply message send to child'
+          reply: 'this is a reply message send to child',
         });
 
-        child.once('message', function (message) {
+        child.once('message', function(message) {
           message.should.eql({
             from: 'child',
             got: {
               from: 'master',
-              reply: 'this is a reply message send to child'
-            }
+              reply: 'this is a reply message send to child',
+            },
           });
           done();
         });
       });
     });
 
-    it('should show warnning message when channel closed', function (done) {
-      var childfile = path.join(__dirname, 'child.js');
-      var child = childprocess.fork(childfile);
-      child.once('message', function (message) {
+    it('should show warnning message when channel closed', function(done) {
+      const childfile = path.join(__dirname, 'child.js');
+      const child = childprocess.fork(childfile);
+      child.once('message', function(message) {
         message.should.eql({
           from: 'child',
-          hi: 'this is a message send to master'
+          hi: 'this is a message send to master',
         });
 
         sendmessage(child, {
           from: 'master',
-          disconnect: true
+          disconnect: true,
         });
 
-        child.once('disconnect', function () {
+        child.once('disconnect', function() {
           console.log('child#%d disconnected', child.pid);
           sendmessage(child, {
             from: 'master',
-            hi: 'here?'
+            hi: 'here?',
           });
           done();
         });
@@ -91,59 +75,59 @@ describe('sendmessage.test.js', function () {
     });
   });
 
-  describe('cluster.fork()', function () {
-    it('should send cross process message', function (done) {
-      var childfile = path.join(__dirname, 'child.js');
+  describe('cluster.fork()', function() {
+    it('should send cross process message', function(done) {
+      const childfile = path.join(__dirname, 'child.js');
       cluster.setupMaster({
-        exec: childfile
+        exec: childfile,
       });
-      var child = cluster.fork();
-      child.once('message', function (message) {
+      const child = cluster.fork();
+      child.once('message', function(message) {
         message.should.eql({
           from: 'child',
-          hi: 'this is a message send to master'
+          hi: 'this is a message send to master',
         });
 
         sendmessage(child, {
           from: 'master',
-          reply: 'this is a reply message send to child'
+          reply: 'this is a reply message send to child',
         });
 
-        child.once('message', function (message) {
+        child.once('message', function(message) {
           message.should.eql({
             from: 'child',
             got: {
               from: 'master',
-              reply: 'this is a reply message send to child'
-            }
+              reply: 'this is a reply message send to child',
+            },
           });
           done();
         });
       });
     });
 
-    it('should show warnning message when channel closed', function (done) {
-      var childfile = path.join(__dirname, 'child.js');
+    it('should show warnning message when channel closed', function(done) {
+      const childfile = path.join(__dirname, 'child.js');
       cluster.setupMaster({
-        exec: childfile
+        exec: childfile,
       });
-      var child = cluster.fork();
-      child.once('message', function (message) {
+      const child = cluster.fork();
+      child.once('message', function(message) {
         message.should.eql({
           from: 'child',
-          hi: 'this is a message send to master'
+          hi: 'this is a message send to master',
         });
 
         sendmessage(child, {
           from: 'master',
-          disconnect: true
+          disconnect: true,
         });
 
-        child.once('disconnect', function () {
+        child.once('disconnect', function() {
           console.log('child#%d disconnected', child.process.pid);
           sendmessage(child, {
             from: 'master',
-            hi: 'here?'
+            hi: 'here?',
           });
           done();
         });
@@ -151,28 +135,28 @@ describe('sendmessage.test.js', function () {
     });
   });
 
-  describe('worker_threads', function () {
-    it('should send cross process message', function (done) {
-      var childfile = path.join(__dirname, 'child.js');
-      const worker = new workerThreads.Worker(childfile)
-      worker.once('message', function (message) {
+  describe('worker_threads', function() {
+    it('should send cross process message', function(done) {
+      const childfile = path.join(__dirname, 'child.js');
+      const worker = new workerThreads.Worker(childfile);
+      worker.once('message', function(message) {
         message.should.eql({
           from: 'child',
-          hi: 'this is a message send to master'
+          hi: 'this is a message send to master',
         });
 
         sendmessage(worker, {
           from: 'master',
-          reply: 'this is a reply message send to child'
+          reply: 'this is a reply message send to child',
         });
 
-        worker.once('message', function (message) {
+        worker.once('message', function(message) {
           message.should.eql({
             from: 'child',
             got: {
               from: 'master',
-              reply: 'this is a reply message send to child'
-            }
+              reply: 'this is a reply message send to child',
+            },
           });
           done();
         });
@@ -180,29 +164,30 @@ describe('sendmessage.test.js', function () {
     });
   });
 
-  it('should emit when SENDMESSAGE_ONE_PROCESS = true', function(done) {
-    var childfile = path.join(__dirname, 'child.js');
-    var child = childprocess.fork(childfile);
-    mm(process.env, 'SENDMESSAGE_ONE_PROCESS', 'true');
-    var msg;
-    child.once('message', function (message) {
-      message.should.eql({
-        from: 'child',
-        hi: 'this is a message send to master'
-      });
+  describe('SENDMESSAGE_ONE_PROCESS', () => {
+    it('should emit when SENDMESSAGE_ONE_PROCESS = true', function(done) {
+      const childfile = path.join(__dirname, 'child.js');
+      const child = childprocess.fork(childfile);
+      mm(process.env, 'SENDMESSAGE_ONE_PROCESS', 'true');
+      child.once('message', function(message) {
+        message.should.eql({
+          from: 'child',
+          hi: 'this is a message send to master',
+        });
 
-      sendmessage(child, {
-        from: 'master',
-        reply: 'this is a reply message send to child'
-      });
-
-      child.once('message', function(msg) {
-        msg.should.eql({
+        sendmessage(child, {
           from: 'master',
           reply: 'this is a reply message send to child',
         });
-        done();
+
+        child.once('message', function(msg) {
+          msg.should.eql({
+            from: 'master',
+            reply: 'this is a reply message send to child',
+          });
+          done();
+        });
       });
     });
-  })
+  });
 });
